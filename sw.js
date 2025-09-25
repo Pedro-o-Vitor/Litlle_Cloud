259b535d0cb4f441f6e5ba89dbd4becf7eaefa4f
Pedro-o-Vitor <ppereira9099@gmail.com>
2025-09-25 17:35:23 -0300

    inicial

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/arrastaresoltar.html',
+  '/telainicial.html',
+  '/publicaeprivada.html',
+  '/protegernuvens.html',
+  '/painelparental.html',
+  '/login.html',
+  '/Jogos.html',
+  '/home.html',
+  '/conectarfios.html',
+  '/cadastro.html',
+  '/style.css',
+    '/tailwind.css',
+  '/cadastro.html',
+  '/arrastaresoltar.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([...new Set(urlsToCache)]); // remove duplicados
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
